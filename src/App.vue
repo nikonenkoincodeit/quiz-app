595fbb1e25538c80bb049644be17399ae3976313
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2024-06-27 22:46:59 +0300

    Added French translation

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ import YourGoal from "./components/YourGoal.vue";
 
 onMounted(() => {
   const { $i18n } = getCurrentInstance().proxy;
-  $i18n.locale = "de";
+  $i18n.locale = "fr";
 });
 
 const items = reactive([
