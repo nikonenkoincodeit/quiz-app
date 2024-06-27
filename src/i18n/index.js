8ee29bee10f7ec6d8319ada3bcef08dbff28ed90
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2024-06-27 17:53:28 +0300

    added Italian translation

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,16 +1,31 @@
 import { createI18n } from "vue-i18n";
 
-// Импортируем переводы
 import en from "./locales/en.json";
 import ru from "./locales/ru.json";
+import cs from "./locales/cs.json";
+import de from "./locales/de.json";
+import es from "./locales/es.json";
+import fr from "./locales/fr.json";
+import it from "./locales/it.json";
+import nl from "./locales/nl.json";
+import pl from "./locales/pl.json";
+import uk from "./locales/uk.json";
 
 const messages = {
   en,
   ru,
+  cs,
+  de,
+  es,
+  fr,
+  it,
+  nl,
+  pl,
+  uk,
 };
 
 export const i18n = createI18n({
-  locale: "en", // Язык по умолчанию
-  fallbackLocale: "en", // Язык, который будет использован, если текущий перевод отсутствует
+  locale: "en",
+  fallbackLocale: "en",
   messages,
 });
